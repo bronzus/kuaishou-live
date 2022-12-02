43fe600b115a69cd2874829aa9e02b6ef921730c
云知易客 <1647762341@qq.com>
2022-12-02 17:49:06 +0800

    增加demo

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import logging
 
-
 from kuaishou import KsLive
-
 
 if __name__ == '__main__':
     LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
     ks = KsLive.Tool()
-    cookie = '你的cookie'
-    url = 'https://live.kuaishou.com/u/haiwangqi'
-    ks.init(url, cookie)
-    # ks.getUserCardInfoById('a604500425')
-    # ks.follow('haiwangqi', 2)
-    # ks.sendMsg("666~", ks.getLiveRoomId())
-    # ks.wssServerStart()
+    ks.init('快手直播地址', '你的快手网页cookie')
+    # 启动快手ws客户端
+    ks.wssServerStart()
